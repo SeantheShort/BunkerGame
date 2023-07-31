84cb0660fbecb22e731254a6da46fd3cdfe6f2ea
SeantheShort <gapammons@gmail.com>
2023-07-31 15:34:08 -0400

    Finished the Player Movement Controller (added Wall Climbing) and added a prefab of it to the folder.

--- BunkerProject/Assets/Scripts/PlayerMovement.cs
+++ BunkerProject/Assets/Scripts/PlayerMovement.cs
@@ -7,26 +7,39 @@ public class PlayerMovement : MonoBehaviour
     // Variables
     float xInput;
     float zInput;
-    public float moveSpeed = 5f;
-    public float gravityScale = -20f;
+    public float moveSpeed;
+    public float gravityScale;
     public bool isGrounded;
     public Vector3 velocity;
-    public float jumpForce = 10f;
-    public float jumpTimer = 1f;
-    public bool hasGlider = true; //CHANGE THIS WHEN ACQUIRED
+    public float jumpForce;
+    public float jumpTimer;
+    public bool hasGlider;
     public bool isGliding;
     public bool isSprinting;
-    public float FOVLerpTime = 0f;
+    public bool isClimbing;
+    public bool climbable;
+    public float climbForce;
+    public float FOVLerpTime;
 
     // Object References
     public CharacterController characterController;
     public Camera mainCamera;
     public Transform groundCheck;
     public LayerMask groundMask;
+    public Transform climbCheck;
+    public LayerMask climbMask;
 
-    // Reference UI Script (so functions can be used)
-    private UIManager UIManager;
-
+    void Start() // Setting values of variables
+    {
+        moveSpeed = 5f;
+        gravityScale = -20f;
+        jumpForce = 10f;
+        jumpTimer = 1f;
+        hasGlider = true; //CHANGE THIS WHEN ACQUIRED
+        climbable = false;
+        climbForce = 3f;
+        FOVLerpTime = 0f;
+    }
 
     void Update()
     {
@@ -37,6 +50,16 @@ public class PlayerMovement : MonoBehaviour
         // Moving Character
         Vector3 movePos = transform.right * xInput + transform.forward * zInput;
         characterController.Move(movePos * moveSpeed * Time.deltaTime);
+
+        // Wall Climbing
+        climbable = Physics.CheckSphere(climbCheck.position, 0.6f, climbMask);
+        if (climbable && zInput > 0f)
+        {
+            isClimbing = true;
+            velocity.y = 0f;
+            characterController.Move(new Vector3(0f, climbForce * Time.deltaTime, 0f));
+        }
+        else { isClimbing = false; }
 
         // Sprinting
         if (Input.GetKey(KeyCode.LeftControl) && !isGliding && zInput != 0f)
@@ -80,17 +103,16 @@ public class PlayerMovement : MonoBehaviour
             gravityScale = -20f;
         }
 
-        // Adaptive FOV (80 to 90)
+        // Adaptive FOV (70 to 75)
         if (isGliding || isSprinting)
         {
-
-            mainCamera.fieldOfView = Mathf.Lerp(80, 90, FOVLerpTime);
-            FOVLerpTime += 7.5f * Time.deltaTime;
+            mainCamera.fieldOfView = Mathf.Lerp(70, 75, FOVLerpTime);
+            FOVLerpTime += 10f * Time.deltaTime;
             FOVLerpTime = Mathf.Min(1, FOVLerpTime);
         }
         else
         { 
-            mainCamera.fieldOfView = 80;
+            mainCamera.fieldOfView = 70;
             FOVLerpTime = 0;
         }
     }
